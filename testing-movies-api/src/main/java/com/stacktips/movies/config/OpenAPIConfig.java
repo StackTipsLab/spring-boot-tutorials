1d39ece476ec654e8b8693e6371f6c1ae96788ba
Nilanchal Panigrahy <nilanchal.panigrahy@tribalworldwide.co.uk>
2024-02-03 21:02:42 +0000

    Misc improvemts

--- testing-movies-api/src/main/java/com/stacktips/movies/config/OpenAPIConfig.java
+++ testing-movies-api/src/main/java/com/stacktips/movies/config/OpenAPIConfig.java
@@ -1,9 +1,7 @@
 package com.stacktips.movies.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,15 +14,9 @@ public class OpenAPIConfig {
         Info apiInfo = new Info().title("Movies API")
                 .description("RESTful API documentation for movies.")
                 .version("1.0");
-//                .license(new License().name("Apache 2.0").url("http://stacktips.com"));
-
 
         return new OpenAPI()
                 .info(apiInfo);
-
-//                .externalDocs(new ExternalDocumentation()
-//                        .description("Movies API Wiki")
-//                        .url("https://github.org/npanigrahy"));
     }
 
 }
