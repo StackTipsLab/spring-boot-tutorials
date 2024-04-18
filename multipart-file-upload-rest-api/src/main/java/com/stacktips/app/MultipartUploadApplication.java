f1767fbd7ee2cdadce2b7ef96c6834898b102ea9
Nilanchala <ask.nilan@gmail.com>
2024-04-18 09:22:29 +0100

    misc

--- multipart-file-upload-rest-api/src/main/java/com/stacktips/app/MultipartUploadApplication.java
+++ multipart-file-upload-rest-api/src/main/java/com/stacktips/app/MultipartUploadApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultipartFileUploadRestApiApplication {
+public class MultipartUploadApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MultipartFileUploadRestApiApplication.class, args);
+        SpringApplication.run(MultipartUploadApplication.class, args);
     }
 
 }
--- multipart-file-upload-rest-api/src/test/java/com/stacktips/app/MultipartUploadApplicationTests.java
+++ multipart-file-upload-rest-api/src/test/java/com/stacktips/app/MultipartUploadApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MultipartFileUploadRestApiApplicationTests {
+class MultipartUploadApplicationTests {
 
 	@Test
 	void contextLoads() {
