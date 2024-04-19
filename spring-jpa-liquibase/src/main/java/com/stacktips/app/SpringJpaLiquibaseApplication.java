6fda2dae7db106ecf0b9fa5a682700dcf8ef0fa5
Nilanchala <ask.nilan@gmail.com>
2024-04-19 12:56:31 +0100

    Added liquibase example:

---
+++ spring-jpa-liquibase/src/main/java/com/stacktips/app/SpringJpaLiquibaseApplication.java
@@ -0,0 +1,12 @@
+package com.stacktips.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaLiquibaseApplication.class, args);
+    }
+}
---
+++ spring-jpa-liquibase/src/main/java/com/stacktips/app/model/ContentRating.java
@@ -0,0 +1,9 @@
+package com.stacktips.app.model;
+
+public enum ContentRating {
+    G,
+    PG,
+    PG13,
+    R,
+    NC17
+}
