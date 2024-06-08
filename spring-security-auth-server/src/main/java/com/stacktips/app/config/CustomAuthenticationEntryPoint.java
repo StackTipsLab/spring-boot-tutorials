0d449d0c96a7166c22825be20d38285e333668e3
Nilanchala Panigrahy <ask.nilan@gmail.com>
2024-06-09 00:45:17 +0100

    Feature/spring security auth server (#2)
    * Added spring security auth server
    
    * Custom error handler

---
+++ spring-security-auth-server/src/main/java/com/stacktips/app/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.stacktips.app.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("error", authException.getMessage());
+        data.put("message", "Custom error message: Invalid client authentication");
+        response.getOutputStream().println(objectMapper.writeValueAsString(data));
+    }
+}
